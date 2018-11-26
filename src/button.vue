af03082650e52dd5d37e7f0bae6a27471fd5ce74
yanjian <yanjian2323@126.com>
2018-11-26 18:50:39 +0800

    按钮基本样式

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,35 @@
 <template>
-    <button class="y-button">按钮</button>
+    <button class="y-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <slot></slot>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        props: {
+            icon: {
+                type: String
+            },
+            iconPosition: {
+                type: String,
+                default: 'left'
+            }
+        }
+    }
 </script>
 <style>
     .y-button {
-
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background-color: var(--button-bg);
+    }
+    .icon {
+        width: 1em;
+        height: 1em;
     }
 </style>
