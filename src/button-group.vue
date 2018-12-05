fe02ae8914246ae56506b3ae702264942848aef6
yanjian <yanjian2323@126.com>
2018-12-05 18:54:08 +0800

    增加button-group

---
+++ src/button-group.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="y-button-group">
+        <slot></slot>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .y-button-group {
+        display: inline-flex;
+        > .y-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
