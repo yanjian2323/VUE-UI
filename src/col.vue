0447478c255d50baf2144bf0cf209ef7661cb24e
yanjian <yanjian2323@126.com>
2018-12-08 22:28:17 +0800

    完成offset偏移的功能

---
+++ src/col.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            span: {
+                type: [String, Number]
+            },
+            offset: {
+                type: [String, Number]
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    $class-prefix: col-;
+    $offset-prefix: offset-;
+    $cols: 24;
+    .col{
+        display: inline-flex;
+        width: 50%;
+        height: 30px;
+        border: 1px solid green;
+
+        @for $n from 1 through $cols {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / $cols) * 100%;
+            }
+        }
+        @for $n from 1 through $cols {
+            &.#{$offset-prefix}#{$n} {
+                margin-left: ($n / $cols) * 100%;
+            }
+        }
+    }
+
+</style>
