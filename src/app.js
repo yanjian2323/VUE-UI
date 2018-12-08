baca83d4894e76a1937010e655bd421786a5c673
yanjian <yanjian2323@126.com>
2018-12-08 22:14:37 +0800

    完成最基本的row和col,根据传入的 列数 生成对应数量的格子

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import Button from './button';
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 import chai from 'chai'
 import spies from 'chai-spies'
@@ -12,6 +14,8 @@ chai.use(spies)
 Vue.component('y-button', Button);
 Vue.component('y-button-group', ButtonGroup)
 Vue.component('y-input', Input)
+Vue.component('y-row', Row)
+Vue.component('y-col', Col)
 new Vue({
     el: '#app',
     data: {
