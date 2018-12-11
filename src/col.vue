19aecf5c9f4b7b3c62f9257ee5448d270745a02e
yanjian <yanjian2323@126.com>
2018-12-11 14:00:58 +0800

    完成响应式布局

--- src/col.vue
+++ src/col.vue
@@ -6,6 +6,28 @@
     </div>
 </template>
 <script>
+    function validator(value) {
+        let isPass = true;
+        const keys = Object.keys(value)
+        keys.forEach((key) => {
+            if (!['span', 'offset'].includes(key)) {
+                isPass = false;
+            }
+        })
+
+        return isPass
+    }
+
+    function createColClasses(obj, device) {
+        const cls = []
+        if (obj) {
+            obj.span && cls.push(`col-${device}${obj.span}`)
+            obj.offset && cls.push(`offset-${device}${obj.offset}`)
+        }
+
+        return cls
+    }
+
     export default {
         props: {
             span: {
@@ -13,19 +35,43 @@
             },
             offset: {
                 type: [String, Number]
+            },
+            phone: {
+                type: Object,
+                validator,
+            },
+            ipad: {
+                type: Object,
+                validator,
+            },
+            narrowPc: {
+                type: Object,
+                validator,
+            },
+            pc: {
+                type: Object,
+                validator,
+            },
+            widePc: {
+                type: Object,
+                validator,
             }
         },
-        data () {
+        data() {
             return {
                 gutter: 0
             }
         },
         computed: {
-            colClass () {
+            colClass() {
                 let cls = [];
-                const { span, offset } = this;
-                this.span && cls.push(`col-${span}`);
-                this.offset && cls.push(`offset-${offset}`);
+                const {span, offset, phone, ipad, narrowPc, pc, widePc} = this;
+                cls.push(createColClasses({ span, offset }, ''))
+                cls.push(createColClasses(phone, 'phone-'))
+                cls.push(createColClasses(ipad, 'ipad-'))
+                cls.push(createColClasses(narrowPc, 'narrow-pc-'))
+                cls.push(createColClasses(pc, 'pc-'))
+                cls.push(createColClasses(widePc, 'wide-pc-'))
 
                 return cls;
             },
@@ -42,7 +88,7 @@
     $class-prefix: col-;
     $offset-prefix: offset-;
     $cols: 24;
-    .col{
+    .col {
         @for $n from 1 through $cols {
             &.#{$class-prefix}#{$n} {
                 width: ($n / $cols) * 100%;
@@ -51,6 +97,62 @@
         @for $n from 1 through $cols {
             &.#{$offset-prefix}#{$n} {
                 margin-left: ($n / $cols) * 100%;
+            }
+        }
+        @media (min-width: 567px) and (max-width: 768px) {
+            $class-prefix: col-ipad-;
+            $offset-prefix: offset-ipad-;
+            @for $n from 1 through $cols {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / $cols) * 100%;
+                }
+            }
+            @for $n from 1 through $cols {
+                &.#{$offset-prefix}#{$n} {
+                    margin-left: ($n / $cols) * 100%;
+                }
+            }
+        }
+        @media (min-width: 769px) and (max-width: 992px) {
+            $class-prefix: col-narrow-pc-;
+            $offset-prefix: offset-narrow-pc-;
+            @for $n from 1 through $cols {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / $cols) * 100%;
+                }
+            }
+            @for $n from 1 through $cols {
+                &.#{$offset-prefix}#{$n} {
+                    margin-left: ($n / $cols) * 100%;
+                }
+            }
+        }
+        @media (min-width: 993px) and (max-width: 1200px) {
+            $class-prefix: col-pc-;
+            $offset-prefix: offset-pc-;
+            @for $n from 1 through $cols {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / $cols) * 100%;
+                }
+            }
+            @for $n from 1 through $cols {
+                &.#{$offset-prefix}#{$n} {
+                    margin-left: ($n / $cols) * 100%;
+                }
+            }
+        }
+        @media (min-width: 1201px) {
+            $class-prefix: col-wide-pc-;
+            $offset-prefix: offset-wide-pc-;
+            @for $n from 1 through $cols {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / $cols) * 100%;
+                }
+            }
+            @for $n from 1 through $cols {
+                &.#{$offset-prefix}#{$n} {
+                    margin-left: ($n / $cols) * 100%;
+                }
             }
         }
     }
